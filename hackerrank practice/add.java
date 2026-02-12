334824c6264eaf59367c6e05bbcfb0a21017bf36
workwithom <om7489874168@gmail.com>
2026-02-12 21:55:53 +0530

    add question

---
+++ hackerrank practice/add.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+
+public class add {
+
+    static int solveMeFirst(int a, int b) {
+        return a+b;
+        
+	}
+
+  
+   public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        in.close();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+	}
+}
